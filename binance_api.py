8eb39e556ac774baf40f10034365220de1f086a4
Sacha <deppdu5758@gmail.com>
2021-02-13 19:30:12 +0100

    add binance api

--- binance_api.py
+++ binance_api.py
@@ -1,15 +1,64 @@
 from binance.client import Client
-import configparser
+from binance.enums import SIDE_BUY, ORDER_TYPE_LIMIT, SIDE_SELL, TIME_IN_FORCE_GTC
+from binance.exceptions import BinanceAPIException
 
-# Read 'conf.ini' file to get binance API credentials and other constants
-parser = configparser.ConfigParser()
-parser.read('conf.ini')
 
-API_KEY = parser.get('binance_credentials', 'api_key')
-API_KEY_SECRET = parser.get('binance_credentials', 'api_key_secret')
+class BinanceApi():
+    
+    def __init__(self, api_key, api_key_secret) -> None:
+        self.client = Client(api_key, api_key_secret)
 
-client = Client(API_KEY, API_KEY_SECRET)
+    def is_close_to_average_price(_, highest_buy_order:float, average_price:float, acceptance:float=0.005) -> bool:
+        return abs((average_price -highest_buy_order) / average_price) < acceptance
 
-# get market depth
-depth = client.get_open_orders()
-print(depth)
+    def get_highest_buy_order(self, binance_pair:str) -> float:
+        return float(self.client.get_order_book(symbol=binance_pair, limit=5)['bids'][0][0])
+
+    def get_average_price(self, binance_pair:str) -> float:
+        return float(self.client.get_avg_price(symbol=binance_pair)['price'])
+
+    def place_buy_order(self, binance_pair:str, quantity:int, price:float, sell_price:float) -> None:
+        try:
+            self.client.create_test_order(
+                symbol=binance_pair,
+                side=SIDE_BUY,
+                type=ORDER_TYPE_LIMIT,
+                timeInForce=TIME_IN_FORCE_GTC,
+                quantity=quantity,
+                price=price
+            )
+
+            print('''buy order successfully placed :
+                     binance_pair -- {}
+                     quantity -- {}
+                     price -- {}'''.format(binance_pair, quantity, price)
+            )
+
+            self.place_sell_order(binance_pair, quantity, sell_price)
+
+        except BinanceAPIException as e:
+            print('Cannot place buy order, check the binance pair and the quantity/price')
+            print(e)
+
+    def place_sell_order(self, binance_pair:str, quantity:int, price:float) -> None:
+        open_orders = self.client.get_open_orders(symbol=binance_pair)
+        if len(open_orders) == 0: # if there is no open order, the buy order has been completed
+            try:
+                self.client.create_test_order(
+                    symbol=binance_pair,
+                    side=SIDE_SELL,
+                    type=ORDER_TYPE_LIMIT,
+                    timeInForce=TIME_IN_FORCE_GTC,
+                    quantity=quantity,
+                    price=price
+                )
+
+                print('''sell order successfully placed :
+                     binance_pair -- {}
+                     quantity -- {}
+                     sell_price -- {}'''.format(binance_pair, quantity, price)
+                )
+
+            except BinanceAPIException as e:
+                print('Cannot place sell order, check the binance pair and the quantity/price')
+                print(e)
--- muskbot.py
+++ muskbot.py
@@ -1,4 +1,8 @@
+from binance_api import API_KEY, API_KEY_SECRET, BinanceApi
 from textblob import TextBlob
+from binance.client import Client
+from binance.enums import SIDE_BUY, ORDER_TYPE_LIMIT, TIME_IN_FORCE_GTC
+from binance.exceptions import BinanceAPIException
 import configparser
 import tweepy
 
@@ -7,61 +11,86 @@ import tweepy
 parser = configparser.ConfigParser()
 parser.read('conf.ini')
 
-API_KEY = parser.get('twitter_credentials', 'api_key')
-API_KEY_SECRET = parser.get('twitter_credentials', 'api_key_secret')
+TWITTER_API_KEY = parser.get('twitter_credentials', 'api_key')
+TWITTER_API_KEY_SECRET = parser.get('twitter_credentials', 'api_key_secret')
 ACCESS_TOKEN = parser.get('twitter_credentials', 'access_token')
 ACCESS_TOKEN_SECRET = parser.get('twitter_credentials', 'access_token_secret')
+
+BINANCE_API_KEY = parser.get('binance_credentials', 'api_key')
+BINANCE_API_KEY_SECRET = parser.get('binance_credentials', 'api_key_secret')
 
 FOLLOWED_USERS = [id for _, id in parser.items('followed_users')]
 
 TRACKED_COINS = {
-    textInTweet:binancePair for binancePair, textsInTweet in parser.items('coins') for textInTweet in textsInTweet.split(', ')
+    text_in_tweet: binance_pair for binance_pair, text_in_tweet in parser.items('coins')
+    for text_in_tweet in text_in_tweet.split(', ')
 }
 
 NEGATIVES_WORDS = {
-    word:polarity for word, polarity in parser.items('negative_words')
+    word: polarity for word, polarity in parser.items('negative_words')
 }
 
 
 # OAuth with Twitter API
-auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+twitter_api = tweepy.API(auth)
+
+# OAuth with Binance API
+binance_api = BinanceApi(API_KEY, API_KEY_SECRET)
 
 
 # Streaming for listening to tweets
-class TweetStreamListener(tweepy.StreamListener):
+def check_sentence_for_coins(sentence: str) -> bool:
+    """Returns true if a string contains any words refering to a followed coins
 
+    Args:
+        sentence (str): the sentence
+
+    Returns:
+        bool: true if the sentence contains a coin
+    """
+    return any(word in sentence.lower() for word in list(TRACKED_COINS.keys()))
+
+
+def is_positive_sentence(sentence: str) -> bool:
+    """Returns true if the sentence is mainly positive
+
+    Args:
+        sentence (str): the sentence
+
+    Returns:
+        bool: true if the sentence is mainly positive
+    """
+    analysis = TextBlob(sentence)
+    custom_polarity = 0
+
+    for word in sentence.split(' '):
+        if word.lower() in NEGATIVES_WORDS:
+            custom_polarity += int(NEGATIVES_WORDS[word])
+
+    return analysis.sentiment.polarity - custom_polarity >= 0
+
+
+class TweetStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         # Skip tweets that are not posted by any of the followed users (replies, retweets from other users ..)
         if status.user.id_str not in FOLLOWED_USERS:
             return
 
-        if (self.check_tweet_for_coins(status.text)):
+        if check_sentence_for_coins(status.text):
             print(status.text)
 
             # Sentiment analysis => only buy if the sentence is positive
-            if (self.is_positive_sentence(status.text)):
+            if is_positive_sentence(status.text):
+                # TODO BUY HERE
                 print('positive')
             else:
                 print('negative')
 
-    def check_tweet_for_coins(_, tweet):
-        return any(coin in tweet.lower() for coin in list(TRACKED_COINS.keys()))
-
-    def is_positive_sentence(_, sentence:str) -> bool:
-        analysis = TextBlob(sentence)
-        custom_polarity = 0
-
-        for word in sentence.split(' '):
-            if (word.lower() in NEGATIVES_WORDS):
-                custom_polarity += int(NEGATIVES_WORDS[word])
-
-        return analysis.sentiment.polarity - custom_polarity >= 0
-
 
 # Start the stream
 tweetStreamListener = TweetStreamListener()
-tweetStream = tweepy.Stream(auth=api.auth, listener=tweetStreamListener)
+tweetStream = tweepy.Stream(auth=twitter_api.auth, listener=tweetStreamListener)
 
 tweetStream.filter(follow=FOLLOWED_USERS, is_async=True)
