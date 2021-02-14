689bdfd156370cc23a726095963765b6cf9a6ed9
Sacha <deppdu5758@gmail.com>
2021-02-14 17:29:17 +0100

    it works now!

--- binance_api.py
+++ binance_api.py
@@ -1,6 +1,8 @@
 from binance.client import Client
 from binance.enums import SIDE_BUY, ORDER_TYPE_LIMIT, SIDE_SELL, TIME_IN_FORCE_GTC
 from binance.exceptions import BinanceAPIException
+from threading import Thread
+from time import sleep
 
 
 class BinanceApi():
@@ -8,16 +10,48 @@ class BinanceApi():
     def __init__(self, api_key, api_key_secret) -> None:
         self.client = Client(api_key, api_key_secret)
 
-    def is_close_to_average_price(_, highest_buy_order:float, average_price:float, acceptance:float=0.005) -> bool:
-        return abs((average_price -highest_buy_order) / average_price) < acceptance
+    def get_usdt_balance(self) -> float:
+        """Returns the USDT balance of the account
+
+        Returns:
+            float: the USDT balance
+        """
+        return float(self.client.get_asset_balance(asset='USDT')['free'])
+
+    def is_close_to_average_price(self, binance_pair:str, highest_buy_order:float, acceptance:float=0.006) -> bool:
+        """Check if the current highest buy order price is close to the average price
+
+        Args:
+            binance_pair (str): the binance pair to check
+            highest_buy_order (float): the current highest buy order price
+            acceptance (float, optional): maximum difference between both prices. Defaults to 0.006.
+
+        Returns:
+            bool: true if the price is close
+        """
+        average_price = float(self.client.get_avg_price(symbol=binance_pair)['price'])
+        return abs((average_price - highest_buy_order) / average_price) < acceptance
 
     def get_highest_buy_order(self, binance_pair:str) -> float:
+        """Returns the highest buy order's price
+
+        Args:
+            binance_pair (str): the binance pair to check
+
+        Returns:
+            float: the highest buy order's price
+        """
         return float(self.client.get_order_book(symbol=binance_pair, limit=5)['bids'][0][0])
 
-    def get_average_price(self, binance_pair:str) -> float:
-        return float(self.client.get_avg_price(symbol=binance_pair)['price'])
-
     def place_buy_order(self, binance_pair:str, quantity:int, price:float, sell_price:float) -> None:
+        """Try to place a buy order, and automaticaly a sell order when the buy order is completed
+
+        Args:
+            binance_pair (str): the binance pair to trade
+            quantity (int): quantity of token to buy
+            price (float): price to buy
+            sell_price (float): price to sell
+        """
         try:
             self.client.create_test_order(
                 symbol=binance_pair,
@@ -34,31 +68,47 @@ class BinanceApi():
                      price -- {}'''.format(binance_pair, quantity, price)
             )
 
-            self.place_sell_order(binance_pair, quantity, sell_price)
+            # Runs on another thread because it will wait until the buy order is completed
+            thread = Thread(target=self.place_sell_order, args=(binance_pair, quantity, sell_price))
+            thread.start()
 
         except BinanceAPIException as e:
             print('Cannot place buy order, check the binance pair and the quantity/price')
-            print(e)
+            if e.code == -1013:
+                print(f'Not enough USDT to purchase ${binance_pair}')
+            else:
+                print(e)
 
     def place_sell_order(self, binance_pair:str, quantity:int, price:float) -> None:
+        """Wait until the corresponding buy order is completed, and then try to place a sell order
+
+        Args:
+            binance_pair (str): the binance pair to trade
+            quantity (int): quantity of token to sell
+            price (float): price to sell
+        """
+        # waits for buy order to complete
         open_orders = self.client.get_open_orders(symbol=binance_pair)
-        if len(open_orders) == 0: # if there is no open order, the buy order has been completed
-            try:
-                self.client.create_test_order(
-                    symbol=binance_pair,
-                    side=SIDE_SELL,
-                    type=ORDER_TYPE_LIMIT,
-                    timeInForce=TIME_IN_FORCE_GTC,
-                    quantity=quantity,
-                    price=price
-                )
+        while len(open_orders) != 0: # if there is no open order, the buy order has been completed
+            sleep(5)
+            open_orders = self.client.get_open_orders(symbol=binance_pair)
 
-                print('''sell order successfully placed :
-                     binance_pair -- {}
-                     quantity -- {}
-                     sell_price -- {}'''.format(binance_pair, quantity, price)
-                )
+        try:
+            self.client.create_test_order(
+                symbol=binance_pair,
+                side=SIDE_SELL,
+                type=ORDER_TYPE_LIMIT,
+                timeInForce=TIME_IN_FORCE_GTC,
+                quantity=quantity,
+                price=price
+            )
 
-            except BinanceAPIException as e:
-                print('Cannot place sell order, check the binance pair and the quantity/price')
-                print(e)
+            print('''sell order successfully placed :
+                    binance_pair -- {}
+                    quantity -- {}
+                    sell_price -- {}'''.format(binance_pair, quantity, price)
+            )
+
+        except BinanceAPIException as e:
+            print('Cannot place sell order, check the binance pair and the quantity/price')
+            print(e)
--- muskbot.py
+++ muskbot.py
@@ -38,16 +38,21 @@ binance_api = BinanceApi(BINANCE_API_KEY, BINANCE_API_KEY_SECRET)
 
 
 # Streaming for listening to tweets
-def check_sentence_for_coins(sentence: str) -> bool:
-    """Returns true if a string contains any words refering to a followed coins
+def check_sentence_for_coins(sentence: str) -> str:
+    """Returns the corresponding binance pair if a string contains any words refering to a followed coins
 
     Args:
         sentence (str): the sentence
 
     Returns:
-        bool: true if the sentence contains a coin
+        str: the binance pair if it contains a coins, otherwise returns 'NO_PAIR'
     """
-    return any(word in sentence.lower() for word in list(TRACKED_COINS.keys()))
+    coin = next((word for word in list(TRACKED_COINS.keys()) if word in sentence.lower()), 'NO_PAIR')
+
+    if coin != 'NO_PAIR':
+        return TRACKED_COINS[coin]
+
+    return coin
 
 
 def is_positive_sentence(sentence: str) -> bool:
@@ -69,21 +74,40 @@ def is_positive_sentence(sentence: str) -> bool:
     return analysis.sentiment.polarity - custom_polarity >= 0
 
 
+def trade_coin(binance_pair:str, percentage_usdt_balance:float) -> None:
+    """Try to initialize a trade
+
+    Args:
+        binance_pair (str): binance pair to trade
+        percentage_usdt_balance (float): percentage of total usdt balance on the account to put on the trade
+    """
+    highest_buy_order = binance_api.get_highest_buy_order(binance_pair)
+
+    if binance_api.is_close_to_average_price(binance_pair, highest_buy_order):
+        usdt_balance = binance_api.get_usdt_balance()
+        
+        quantity = round((usdt_balance / highest_buy_order) * percentage_usdt_balance, 8) # rounds up to 8 digits, see binance api max precision
+
+        binance_api.place_buy_order(binance_pair, quantity, highest_buy_order, sell_price=highest_buy_order * 1.15) # 15% benefits
+    
+    else:
+        print(f'Too much volatility on the pair ${binance_pair}')
+
+
 class TweetStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         # Skip tweets that are not posted by any of the followed users (replies, retweets from other users ..)
         if status.user.id_str not in FOLLOWED_USERS:
             return
 
-        if check_sentence_for_coins(status.text):
-            print(status.text)
+        binance_pair = check_sentence_for_coins(status.text)
 
+        if binance_pair != 'NO_PAIR':
             # Sentiment analysis => only buy if the sentence is positive
             if is_positive_sentence(status.text):
-                # TODO BUY HERE
-                print('positive')
+                trade_coin(binance_pair.upper(), 0.1)  # 10% in
             else:
-                print('negative')
+                print('negative sentence')
 
 
 # Start the stream
